874e81988474c45ceb2233c6a26aa08c100653ff
wangechimk <wangechik.michelle@gmail.com>
2021-05-12 15:32:29 +0300

    add bootstrap and image library

--- Gallery/settings.py
+++ Gallery/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'picha.apps.PichaConfig',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
