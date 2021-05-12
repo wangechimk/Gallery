93ce5f3e55de842a8ac498400abdb7c069314f76
wangechimk <wangechik.michelle@gmail.com>
2021-05-12 20:36:38 +0300

    add image categories model

--- picha/models.py
+++ picha/models.py
@@ -5,3 +5,12 @@ class Images(models.Model):
     image_link = models.ImageField(upload_to='images/')
     title = models.CharField(max_length=80)
     description = models.TextField()
+#add image categories model
+    def __str__(self):
+        return self.title
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name 
