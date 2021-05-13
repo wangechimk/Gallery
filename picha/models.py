5f3b7d93fbab86e5abe40418c6c7d425231fad03
wangechimk <wangechik.michelle@gmail.com>
2021-05-13 20:33:14 +0300

    add locatins model

--- picha/models.py
+++ picha/models.py
@@ -12,6 +12,15 @@ class Images(models.Model):
     title = models.CharField(max_length=80)
     description = models.TextField()
     category=models.ForeignKey(Category, on_delete=models.CASCADE)
+    Location = models.ForeignKey('Locations', on_delete=models.CASCADE, default=1)
+
 # add image categories model
     def __str__(self):
         return self.title
+
+class Locations(models.Model):
+    city = models.CharField(max_length=30)
+    country = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.city
