135911e57c1c6cf6149d6a96ac6f5b7fb66f0340
wangechimk <wangechik.michelle@gmail.com>
2021-05-15 01:09:30 +0300

    initialize search based on category

--- picha/urls.py
+++ picha/urls.py
@@ -5,5 +5,6 @@ from django.conf.urls.static import static
 app_name='picha'
 urlpatterns=[
     path('', views.homepage, name='landing'), 
-    path('gallery/', views.gallery, name='gallery')
+    path('gallery/', views.gallery, name='gallery'),
+    path('search/', views.search, name='search')
 ]
--- picha/views.py
+++ picha/views.py
@@ -9,3 +9,14 @@ def homepage(request):
 def gallery(request):
     pictures = Images.get_all()
     return render(request, 'gallery.html', {'pictures': pictures})
+
+def search(request):
+    if 'category' in request.GET and request.GET['category']:
+        search_term = request.GET.get('category')
+        res = Images.search_image(search_term)
+        message = f'{search_term}'
+
+        return render(request, 'search.html', {'message':message, 'results':res})
+    else:
+        message = 'You have not searched any term'
+        return render(request, 'search.html', {'message':message}) 
